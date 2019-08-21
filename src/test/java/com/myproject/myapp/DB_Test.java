55981e583f35fb64b3055592aeaa34d793cee454
oksujin <ok0579@naver.com>
2019-08-21 16:54:50 +0900

    #2

--- src/test/java/com/myproject/myapp/DB_Test.java
+++ src/test/java/com/myproject/myapp/DB_Test.java
@@ -9,8 +9,18 @@ public class DB_Test {
 public void test() throws Exception {
     Class.forName("org.mariadb.jdbc.Driver"); // 마리아DB
 
-    Connection con = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/jspdb", "root",     "sue013126*");// 마리아DB
-
+    Connection con = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/jspdb", "root",     "PW");// 마리아DB
+    //jspdb : 테스트를 위해 HeidiSQL에서 생성한 테이블의 이름
+    //root : HeidiSQL에서 생성한 세션 이름
+    //PW : mariaDB 설치시 설정한 비밀번호
+    
     System.out.println(con);
     }
 }
+
+/*
+ * DB연동 테스트 코드입니다
+ * JUnit으로 실행하여 Error가 나지 않고, Console창에
+ * org.mariadb.jdbc.MariaDbConnection@270421f5
+ * 문구가 출력된다면 성공입니다.
+ */
